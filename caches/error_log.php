75239a2df9bfe860d62b7c104d8c3faa4deed6a0
Noble.Yu <Noble.Yu@mullenloweprofero.com>
2018-01-08 11:29:27 +0800

    update

---
+++ caches/error_log.php
@@ -0,0 +1,37 @@
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>01-08 11:19:55 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>01-08 11:19:55 | 2 | mysqli::mysqli(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) | phpcms\libs\classes\db_mysqli.class.php | 55
+<?php exit;?>01-08 11:19:55 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>01-08 11:19:55 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>01-08 11:19:55 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>01-08 11:19:55 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>01-08 11:19:55 | 2 | mysqli::close(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 409
+<?php exit;?>01-08 11:21:44 | 2 | mysqli_connect(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) | C:\study\wamp\www\phpcms\install\install.php | 359
---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://localhost/phpcms/',
+    'authkey' => 'iU7AFfBVAUrh4nyDw6ST8vSd0c7mvBoG',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'gbk',
+    'synlogin' => '1',
+  ),
+);
+?>
